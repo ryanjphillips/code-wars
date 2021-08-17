952e94a9f873dfbb81aab17962de1aef5478d5a1
Ryan Phillips <ryan.phillips.j@gmail.com>
2021-08-16 23:34:27 -0500

    Disemvowel Trolls

---
+++ disemvowelTrolls.js
@@ -0,0 +1,9 @@
+//Author: Ryan Philiips
+//Date: August 16th, 2021
+//Kyu: 7
+//Link: https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript
+//Profile: https://www.codewars.com/users/yourSolution
+
+function disemvowel(str) {
+    return str.replace(/a|i|o|u|e/gi, "");
+  }
